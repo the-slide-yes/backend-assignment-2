b351ad832c76fc742811c4ee3bc685e34f2e37d1
Aburnett2-rrc <aburnett2@Academic.RRC.CA>
2025-09-20 14:33:20 -0500

    feat: add employee routes

---
+++ src/api/v1/routes/employeeRoutes.ts
@@ -0,0 +1,19 @@
+import express, { Router } from "node_modules/@types/express";
+import { 
+    createEmployee, 
+    deleteEmployee, 
+    getAllEmployees, 
+    getEmployeeById, 
+    updateEmployee 
+} from "../controllers/employeeController";
+
+
+const router: Router = express.Router();
+
+router.get("/employees", getAllEmployees);
+router.get("/employees/:id", getEmployeeById);
+router.post("/employees", createEmployee);
+router.put("/employees/:id", updateEmployee);
+router.delete("/employees/:id", deleteEmployee);
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
 import morgan from "morgan";
-
+import employeeRoutes from "./api/v1/routes/employeeRoutes";
 
 // Initialize Express application
 const app: Express = express();
@@ -12,6 +12,8 @@ app.use(morgan("combined"));
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
+app.use("/api/v1", employeeRoutes);
 
 app.get("/api/v1/health", (req, res) => {
     res.json({
