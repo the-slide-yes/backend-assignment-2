f7bfa32aa5a5f7a9d559b557d87a2d330bc63a5e
Aburnett2-rrc <aburnett2@Academic.RRC.CA>
2025-11-04 21:16:41 -0600

    feat: add swagger options

---
+++ config/swaggerOptions.ts
@@ -0,0 +1,42 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const serverUrl: string =
+    process.env.SWAGGER_SERVER_URL || "http://localhost:3000/api/v1";
+
+const swaggerOptions: swaggerJsdoc.Options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Employee Tracking application API Documentation",
+            version: "1.0.0",
+            description:
+                "This is the API documentation for the Employee Tracking application which tracks employees.",
+        },
+        servers: [
+            {
+                url: serverUrl,
+                description: process.env.NODE_ENV,
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    // Path to the API docs and schemas
+    apis: ["./src/api/v1/routes/*.ts", "./src/api/v1/validations/*.ts"],
+};
+
+export const generateSwaggerSpec: () => object = (): object => {
+    return swaggerJsdoc(swaggerOptions);
+};
